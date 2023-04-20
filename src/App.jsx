07efe6b9502e5100c1a4bb821c66619079e59231
Naomi Cherotich <naomirono1454@gmail.com>
2023-04-20 22:19:05 +0300

    Import landingPage and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import LandingPage from './components/LandingPage'
+import Navbar from './components/Navbar'
+
 
 function App() {
 
   return (
     <div className="App">
-      
+    <Navbar/>
+      <LandingPage/>
     </div>
   )
 }

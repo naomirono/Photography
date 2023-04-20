386fb5ca53752c071cb695c5c3b114a4522a333a
Naomi Cherotich <naomirono1454@gmail.com>
2023-04-20 22:16:22 +0300

    Add custom Roboto font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['Roboto', 'Arial', 'sans-serif'],
+    }
   },
   plugins: [],
 }

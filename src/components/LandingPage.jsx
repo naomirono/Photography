ccbd65c228d2bc56c1b20ac576b5c7dea2fe2bf5
Naomi Cherotich <naomirono1454@gmail.com>
2023-04-23 23:11:10 +0300

    add padding to the left to h1, h6, p

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -14,14 +14,14 @@ const LandingPage = () => {
       }}>
       
       <div className="w-1/2 flex flex-col justify-center bg-black bg-opacity-70 text-start">
-           <h1 className="text-6xl font-bold mb-5 text-white mt-20 pl-10">PHOTOGRAPHY</h1>
-           <h1 className="text-5xl font-bold mb-10 text-orange-400 pl-10">STUDIO</h1>
-           <h6 className="text-3xl font-medium  text-gray-500  pl-10">For All Services</h6>
-           <p className=" text-gray-400 m-10 text-lg">
+           <h1 className="text-6xl font-bold mb-5 text-white mt-20 pl-16">PHOTOGRAPHY</h1>
+           <h1 className="text-5xl font-bold mb-10 text-orange-400 pl-16">STUDIO</h1>
+           <h6 className="text-3xl font-medium  text-white  pl-16 pb-6">For All Services</h6>
+           <p className=" text-gray-400 ml-16 text-lg">
              Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae velit vel arcu ultricies lobortis. Donec quis consectetur purus. Nam in pharetra ipsum, vel placerat eros. 
             </p>
         
-         <div className="flex justify-between mt-20 m-8">
+         <div className="flex justify-between mt-20 ml-16">
             <button className="bg-orange-400 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded-3xl mb-4">Book Now</button>
         <div className='flex justify-between'>
             <FaFacebook className="text-4xl text-blue-500 hover:text-blue-700 cursor-pointer m-4 bg-white rounded-full p-1" />

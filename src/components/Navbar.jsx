5ab6e331b10145b14822bd4c582ba5d2c8cb2623
Naomi Cherotich <naomirono1454@gmail.com>
2023-04-23 22:49:00 +0300

    change navlink texts to orange-400

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,18 +8,18 @@ function Navbar() {
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-14">
           <div className="flex-shrink-0 flex items-center">
-            <img className="h-16 w-22 mr-2 brightness-150" src={LogoImage} alt="Logo Image" />
+            <img className="h-16 w-22 mr-2 filter contrast-125 brightness-110" src={LogoImage} alt="Logo Image" />
             <Link to="/" className="text-2xl font-bold">
-              <span className="text-gray-400">PHOTO</span><span className="text-yellow-300">STUDIO</span>
+              <span className="text-gray-400">PHOTO</span><span className="text-orange-400">STUDIO</span>
             </Link>
           </div>
           <div className="hidden md:block">
             <div className="ml-10 flex items-baseline space-x-4">
-              <Link to="/" className="text-[18px] hover:border-b-2 border-yellow-300 hover:text-yellow-300 px-3 py-2 rounded-md text-sm font-medium text-yellow-300 ">Home</Link>
-              <Link to="/portfolio" className="text-[18px] text-gray-400 hover:border-b-2 border-yellow-300 hover:text-yellow-300  px-3 py-2 rounded-md text-sm font-medium">Portfolio</Link>
-              <Link to="/services" className="text-[18px] text-gray-400 hover:border-b-2 border-yellow-300 hover:text-yellow-300  px-3 py-2 rounded-md text-sm font-medium">Services</Link>
-              <Link to="/pricing" className="text-[18px] text-gray-400 hover:border-b-2 border-yellow-300 hover:text-yellow-300  px-3 py-2 rounded-md text-sm font-medium">Pricing</Link>
-              <Link to="/contact" className="text-[18px] text-gray-400 hover:border-b-2 border-yellow-300 hover:text-yellow-300  px-3 py-2 rounded-md text-sm font-medium">Contact</Link>
+              <Link to="/" className="text-[18px] text-orange-400 hover:border-b-2 border-orange-400 hover:text-orange-400 px-3 py-2 rounded-md text-sm font-medium">Home</Link>
+              <Link to="/portfolio" className="text-[18px] text-gray-400 hover:border-b-2 border-orange-400 hover:text-orange-400  px-3 py-2 rounded-md text-sm font-medium">Portfolio</Link>
+              <Link to="/services" className="text-[18px] text-gray-400 hover:border-b-2 border-orange-400 hover:text-orange-400  px-3 py-2 rounded-md text-sm font-medium">Services</Link>
+              <Link to="/pricing" className="text-[18px] text-gray-400 hover:border-b-2 border-orange-400 hover:text-orange-400  px-3 py-2 rounded-md text-sm font-medium">Pricing</Link>
+              <Link to="/contact" className="text-[18px] text-gray-400 hover:border-b-2 border-orange-400 hover:text-orange-400  px-3 py-2 rounded-md text-sm font-medium">Contact</Link>
             </div>
           </div>
         </div>

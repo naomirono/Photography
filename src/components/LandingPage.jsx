3a310222e6342c764ae5721e3ef0aa884f69f450
Naomi Cherotich <naomirono1454@gmail.com>
2023-04-25 22:11:29 +0300

    update code to make it responsive

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -6,40 +6,39 @@ import BgImage from '../assets/IMG_9791.JPG';
 
 const LandingPage = () => {
   return (
-    <div className="flex justify-between h-[89vh] background  bg-cover bg-no-repeat"  style={{
-        backgroundImage: `url(${BgImage})`,
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        
-      }}>
-      
-      <div className="w-1/2 flex flex-col justify-center bg-black bg-opacity-70 text-start">
-           <h1 className="text-6xl font-bold mb-5 text-white mt-20 pl-16">PHOTOGRAPHY</h1>
-           <h1 className="text-5xl font-bold mb-10 text-orange-400 pl-16">STUDIO</h1>
-           <h6 className="text-3xl font-medium  text-white  pl-16 pb-6">For All Services</h6>
-           <p className=" text-gray-400 ml-16 text-lg">
-             Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae velit vel arcu ultricies lobortis. Donec quis consectetur purus. Nam in pharetra ipsum, vel placerat eros. 
-            </p>
-        
-         <div className="flex justify-between mt-20 ml-16">
-            <button className="bg-orange-400 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded-3xl mb-4">Book Now</button>
-        <div className='flex justify-between'>
-            <FaFacebook className="text-4xl text-blue-500 hover:text-blue-700 cursor-pointer m-4 bg-white rounded-full p-1" />
-            <FaTwitter className="text-4xl text-blue-500 hover:text-blue-700 cursor-pointer m-4 bg-white rounded-full p-1" />
-            <FaInstagram className="text-4xl text-pink-500 hover:text-pink-700 cursor-pointer m-4 bg-white rounded-full p-1" />
-            <FaYoutube className="text-4xl text-red-500 hover:text-red-700 cursor-pointer m-4 bg-white rounded-full p-1" />
-        </div>
-  
-            
-  
-          </div>
+    <div className="flex flex-col-reverse md:flex-row justify-between h-auto md:h-[89vh] background bg-cover bg-no-repeat" style={{
+      backgroundImage: `url(${BgImage})`,
+      backgroundPosition: "center",
+      backgroundSize: "cover",
+    }}>
+      <div className="w-full md:w-1/2 flex flex-col justify-center bg-black bg-opacity-70 text-start">
+        <h1 className="text-4xl md:text-6xl font-bold mb-5 text-white mt-10 ml-6 md:mt-20 sm:pl-10">PHOTOGRAPHY</h1>
+        <h1 className="text-3xl md:text-5xl font-bold mb-10 text-orange-400 ml-6 sm:pl-10">STUDIO</h1>
+        <h6 className="text-2xl md:text-3xl font-medium text-white pl-6 md:pl-16 pb-6">For All Services</h6>
+        <p className="text-gray-400 ml-6 md:ml-16 text-lg md:text-xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae velit vel arcu ultricies lobortis. Donec quis consectetur purus. Nam in pharetra ipsum, vel placerat eros.
+        </p>
 
+        <div className="flex flex-col md:flex-row justify-between items-center mt-10 md:mt-20 ml-6 md:ml-16">
+          <button className="bg-orange-400 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded-3xl mb-4 md:mb-0">Book Now</button>
+          <div className='flex mt-4 md:mt-0'>
+            <FaFacebook className="text-2xl md:text-4xl text-blue-500 hover:text-blue-700 cursor-pointer m-2 md:m-4 bg-white rounded-full p-1" />
+            <FaTwitter className="text-2xl md:text-4xl text-blue-500 hover:text-blue-700 cursor-pointer m-2 md:m-4 bg-white rounded-full p-1" />
+            <FaInstagram className="text-2xl md:text-4xl text-pink-500 hover:text-pink-700 cursor-pointer m-2 md:m-4 bg-white rounded-full p-1" />
+            <FaYoutube className="text-2xl md:text-4xl text-red-500 hover:text-red-700 cursor-pointer m-2 md:m-4 bg-white rounded-full p-1" />
+          </div>
         </div>
-      <div className="w-1/2 flex flex-col justify-center bg-black bg-opacity-70 ">
-        <img src={CameraImage} alt="Camera" className="w-full" style={{ height: '600px' }}/>
+      </div>
+      <div className="w-full md:w-1/2 flex flex-col justify-center bg-black bg-opacity-70">
+        <img src={CameraImage} alt="Camera" className="w-full h-auto md:h-full" style={{ maxHeight: '600px' }} />
       </div>
     </div>
   );
 };
+
+
+
+
+
 
 export default LandingPage;

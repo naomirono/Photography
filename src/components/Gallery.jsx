b2babc7f61b793d2d348bc90a4a3c1ca2133d1bb
Naomi Cherotich <naomirono1454@gmail.com>
2023-04-27 23:41:08 +0300

    Create a Gallery component to display portfolio images

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import portraitPhoto from '../assets/POT1.JPG';
+
+const photos = [
+  { src: portraitPhoto, width: 'w-1/2', height: 'h-64' },
+  { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
+  { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
+  { src: portraitPhoto, width: 'w-1/2', height: 'h-64' },
+  { src: portraitPhoto, width: 'w-1/2', height: 'h-64' },
+  { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
+  { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
+  { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
+  { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
+  { src: portraitPhoto, width: 'w-1/2', height: 'h-64' },
+  { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
+  { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
+  { src: portraitPhoto, width: 'w-1/2', height: 'h-64' },
+  { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
+  { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
+];
+
+const Gallery = () => {
+  return (
+    <div className="flex flex-wrap justify-center items-center gap-4">
+      {photos.map((photo) => (
+        <img
+          key={photo.src}
+          src={photo.src}
+          alt=""
+          className={`object-cover ${photo.width} ${photo.height}`}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Gallery;

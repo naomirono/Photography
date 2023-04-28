2e09b463f154fd30036005f14965336fafacb953
Naomi Cherotich <naomirono1454@gmail.com>
2023-04-28 20:39:33 +0300

    Change the width and height of images

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,26 +3,22 @@ import portraitPhoto from '../assets/POT1.JPG';
 import familyPortrait from '../assets/F1.JPG';
 
 const photos = [
-  { src: portraitPhoto, width: 'w-1/2', height: 'h-64' },
+  { src: portraitPhoto, width: 'w-1/2', height: 'h-96' },
   { src: familyPortrait, width: 'w-1/4', height: 'h-96' },
   { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
-  { src: portraitPhoto, width: 'w-1/2', height: 'h-64' },
-  { src: portraitPhoto, width: 'w-1/2', height: 'h-64' },
   { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
   { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
   { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
-  { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
-  { src: portraitPhoto, width: 'w-1/2', height: 'h-64' },
-  { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
-  { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
-  { src: portraitPhoto, width: 'w-1/2', height: 'h-64' },
+  { src: portraitPhoto, width: 'w-1/2', height: 'h-96' },
   { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
   { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
+  { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
+  { src: portraitPhoto, width: 'w-3/4', height: 'h-96' },
 ];
 
 const Gallery = () => {
   return (
-    <div className="flex flex-wrap justify-center items-center gap-4">
+    <div className="flex flex-wrap justify-center items-center gap-4 my-8">
       {photos.map((photo) => (
         <img
           key={photo.src}

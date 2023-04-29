e8d4969d3ac88be13a7713bf4c9444f0a5c4f5e5
Naomi Cherotich <naomirono1454@gmail.com>
2023-04-29 20:15:58 +0300

    Change images to display

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import portraitPhoto from '../assets/POT1.JPG';
-import familyPortrait from '../assets/F1.JPG';
+import Photo2 from '../assets/F1.JPG';
+import Photo3 from '../assets/F2.JPG';
+import Photo4 from '../assets/POT1.JPG';
+import Photo5 from '../assets/F4.JPG';
+import Photo6 from '../assets/w5.JPG';
+import Photo7 from '../assets/w6.JPG';
+
 
 const photos = [
-  { src: portraitPhoto, width: 'w-1/2', height: 'h-96' },
-  { src: familyPortrait, width: 'w-1/4', height: 'h-96'},
-  { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
-  { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
-  { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
-  { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
-  { src: portraitPhoto, width: 'w-1/2', height: 'h-96' },
-  { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
-  { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
-  { src: portraitPhoto, width: 'w-1/4', height: 'h-96' },
-  { src: portraitPhoto, width: 'w-3/4', height: 'h-96' },
+  { src: Photo7, width: 'w-1/2', height: 'h-96' },
+  { src: Photo2, width: 'w-1/4', height: 'h-96'},
+  { src: Photo3, width: 'w-1/4', height: 'h-96' },
+  { src: Photo4, width: 'w-1/4', height: 'h-96' },
+  { src: Photo5, width: 'w-1/4', height: 'h-96' },
+  { src: Photo4, width: 'w-1/4', height: 'h-96' },
+  { src: Photo6, width: 'w-1/2', height: 'h-96' },
+  { src: Photo4, width: 'w-1/4', height: 'h-96' },
+  { src: Photo4, width: 'w-1/4', height: 'h-96' },
+  { src: Photo4, width: 'w-1/4', height: 'h-96' },
+  { src: Photo7, width: 'w-1/2', height: 'h-96' },
+  { src: Photo5, width: 'w-1/4', height: 'h-96' },
 ];
 
 const Gallery = () => {

058b1b210c0b16fa6918c1c2df42736942d4ba23
Naomi Cherotich <naomirono1454@gmail.com>
2023-04-29 20:14:39 +0300

    Import footer component to render it

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import Services from './components/Services'
 import { Routes, Route } from "react-router-dom";
 import Gallery from './components/Gallery';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -18,9 +19,11 @@ function App() {
     <Route path="/services" element={<Services/>} />
 
     <Route path="/portfolio" element={<Gallery/>} />
+
+    
     </Routes>
     
-      
+      <Footer/>
       
       
     </div>

2799ae81d5a70417139e238ebae158f9bba354ba
Naomi Cherotich <naomirono1454@gmail.com>
2023-04-30 15:52:18 +0300

    Add Price range to each service

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -14,36 +14,42 @@ function Services() {
       title: 'Wedding Photography',
       description: 'We specialize in capturing the magic and beauty of your special day. Our experienced photographers will work with you to create stunning images that you will cherish for a lifetime.',
       image: weddingPhoto,
+      priceRange:"$1000-$5000",
       link: '/services/wedding'
     },
     {
       title: 'Family Portraits',
       description: 'Our family portrait sessions are designed to capture the love and joy that your family shares. We will work with you to create beautiful, natural images that you will treasure for years to come.',
       image: familyPortrait,
+      priceRange:"$200-$1000",
       link: '/services/family'
     },
     {
       title: 'Commercial Photography',
       description: 'We offer a range of commercial photography services, including product photography, headshots, and event coverage. Our photographers will work closely with you to create images that showcase your brand and tell your story.',
       image: commercialPhoto,
+      priceRange:"$100-$2000",
       link: '/services/commercial'
     },
     {
       title: 'Engagement Photography',
       description: 'Our engagement photography sessions are designed to capture the love and connection between you and your partner. We will work with you to create images that are unique to your relationship and showcase your personalities.',
       image: engagementPhoto,
+      priceRange:"$1000-$5000",
       link: '/services/engagement'
     },
     {
       title: 'Event Photography',
       description: 'From birthday parties to corporate events, we offer event photography services that capture the excitement and energy of your special occasion. Our photographers will work tirelessly to ensure that every moment is captured.',
       image: eventPhoto,
+      priceRange:"$1000-$5000",
       link: '/services/event'
     },
     {
       title: 'Portrait Photography',
       description: 'Our portrait photography sessions are designed to capture the unique personality of each individual. From high school seniors to professionals, we will work with you to create stunning images that showcase your style and personality.',
       image: portraitPhoto,
+      priceRange:"$100-$800",
       link: '/services/portrait'
     },
   ];
@@ -74,6 +80,13 @@ function Services() {
             </div>
             <h3 className="text-lg font-bold mb-2 mt-[8px] text-black">{service.title}</h3>
             <p className="text-gray-700 text-base flex-1">{service.description}</p>
+
+            <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+        {service.priceRange}
+        </span>
+      </div>
+
 
           </Link>
         </div>

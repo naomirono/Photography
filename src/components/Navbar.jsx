991e898bc1c0d7f17268cd033b3455d1106ed68d
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-01 18:42:15 +0300

    Update code to make navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
   const toggleMenu = () => setShowMenu(!showMenu);
 
   return (
-    <nav className="bg-black p-4">
+    <nav className=" fixed top-0 left-0 right-0 bg-black p-4">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-14">
           <div className="flex-shrink-0 flex items-center">
